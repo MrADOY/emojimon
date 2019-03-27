692fb48be7608c84d3f3405533162763bd75bbd8
Aurelien Pietrzak <pietrzak.aurelien@gmail.com>
2019-03-27 09:43:41 +0100

    add dataset

---
+++ dataset/normalize.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+size="256x256"
+
+for dir in $(ls -d */); do
+  echo 'Entering' $dir
+  cd $dir
+
+  echo -e "Removing normalized images from previous iteration (if any)... \c"
+  rm -f normalized-*.jpg
+  echo "Done."
+
+  for file in $(ls *.jpg); do
+    echo -e "Normalizing $file... \c"
+    convert $file -resize $size^ -gravity center -extent $size "normalized-$file"
+    echo "Done."
+  done
+
+  cd ..
+  echo 'Leaving' $dir
+  echo "Success"
+done
