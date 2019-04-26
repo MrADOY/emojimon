7b86e1613a77c25dc44d3db1be5807d7d3ec4baa
Sadgasmification <nicolas.jourdain@etu.uphf.fr>
2019-04-26 10:11:13 +0200

    Add canvas instead of video tag

--- webcam.js
+++ webcam.js
@@ -3,51 +3,81 @@
 Marceau Hollertt, Aurelien Pietrzak.
 */
 
-// When it's load.
-
 const IMAGE_WIDTH = 100;
 const IMAGE_HEIGHT = 100;
 const IMAGE_CHANNELS = 3;
 const outShape = [1, IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS];
 
+var rotation = 0,
+loopFrame,
+centerX,
+centerY;
+
 $(() => {
-  $('.video .cam').hide();
+  $('.video .cam_hidden').hide();
   $('.video .error').hide();
+
+  var canvas = $('#canvas')[0];
+  var ctx = $('#canvas')[0].getContext('2d');
   const video = $('.video .cam video')[0];
+  const video_hidden = $('.video .cam_hidden video')[0];
 
   const constraints = {
     audio: false,
     video: {
-      width: {
-        exact: 256
-      },
-      height: {
-        exact: 256
-      }
+      width: 256,
+      height: 256,
+      facingMode: "environment"
     }
   };
 
+  video_hidden.addEventListener('loadedmetadata',function(){
+    width = canvas.width = video_hidden.videoWidth;
+    height = canvas.height = video_hidden.videoHeight;
+    centerX = width / 2;
+    centerY = height / 2;
+    startLoop();
+  });
+
   // Get access to webcam
-  navigator.mediaDevices.getUserMedia(constraints)
-  .then(handleSuccess)
-  .catch(handleError);
+  navigator.getUserMedia = ( navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia);
 
-  // Stream the video.
-  function handleSuccess(stream) {
-    video.srcObject = stream;
-    setTimeout(function () {
-      $('.video .cam').show('slow');
-    }, 500);
-    return tf.browser.fromPixels($('video')[0]);
-  }
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia(constraints,handleSuccess,handleError);
+    }
 
-  // When error
-  function handleError(error) {
-    console.error(error);
-    $('.video .error').prepend('<div class="alert alert-danger" role="alert"><strong>Erreur</strong> La caméra est obligatoire. </div>');
-    $('.video .error').show('slow');
-  }
-});
+    // Stream the video.
+    function handleSuccess(stream) {
+      video.srcObject = stream;
+      video_hidden.srcObject = stream;
+    }
+
+    // When error
+    function handleError(error) {
+      console.error(error);
+      $('.video .error').prepend('<div class="alert alert-danger" role="alert"><strong>Erreur</strong> La caméra est obligatoire. </div>');
+      $('.video .error').show('slow');
+    }
+
+    function loop(){
+      loopFrame = requestAnimationFrame(loop);
+      ctx.save();
+      draw(video_hidden, canvas, ctx, 30);
+      ctx.restore();
+    }
+
+    function draw(video, canvas, context, frameRate) {
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      setTimeout(draw, 1/frameRate, video, canvas, context, frameRate);
+    }
+
+    function startLoop(){
+      loopFrame = loopFrame || requestAnimationFrame(loop);
+    }
+  });
 
   // Captures a frame from the webcam and convert it to tensor.
   function capture() {
